3ca6bb0a2b313c9e3065cc6b18a80ea758b8660b
iclal millik <icllmllk03@gmail.com>
2023-11-30 19:55:30 +0300

    init

---
+++ prolab2deneme1/FirmaKaydıPaneli.cs
@@ -0,0 +1,63 @@
+﻿using Prolab2_1.deneme;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prolab2deneme1
+{
+    public partial class FirmaKaydıPaneli : Form
+    {
+        public FirmaKaydıPaneli()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        public void FirmaKaydıPaneli_Load(object sender, EventArgs e)
+        { /*
+            string textveri= txtveri.Text;
+            Firma firma = new Firma();                   //hata var çözemedim yorum satırlarını kaldırırsanız firmakaydıpanelini açmıyor
+            firma.firmaEkle(Firma r);
+           */  
+            }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+         
+
+        
+        }
+
+       
+       
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FirmalariGoruntule firmalariGoruntule=new FirmalariGoruntule();
+            //firmalariGoruntule.Owner = this;
+            firmalariGoruntule.Show();
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            //.listBox1.Items.Add(txtveri.Text);
+        }
+
+        public void txtveri_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ prolab2deneme1/FirmalariGoruntule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prolab2deneme1
+{
+    public partial class FirmalariGoruntule : Form 
+    {
+        public FirmalariGoruntule(List<string> firmalarListesi)
+        {
+        }
+
+        public FirmalariGoruntule(string eklenenfirma)
+        {
+            InitializeComponent();
+
+           
+
+
+
+        }
+
+        public FirmalariGoruntule()
+        {
+        }
+
+        private void FirmalariGoruntule_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+    }
+}
